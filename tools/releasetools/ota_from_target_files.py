153ccc89ae54ca3b1eeef67c85932d41365cb94c
rpingat <pingatravi@gmail.com>
2020-03-02 19:57:47 +0530

    build/make: Refactor for AIM
    Change-Id: I2b806e51cbe7d25804e51e81a44205bc61f04c50

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -804,6 +804,19 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+
+  #Print ASCII
+  script.Print("********************************************");
+  script.Print("    ___    _           ____                 ");
+  script.Print("   /   |  (_)___ ___  / __ \____  ____ ___  ");
+  script.Print("  / /| | / / __ `__ \/ /_/ / __ \/ __ `__ \ ");
+  script.Print(" / ___ |/ / / / / / / _, _/ /_/ / / / / / / ");
+  script.Print("/_/  |_/_/_/ /_/ /_/_/ |_|\____/_/ /_/ /_/  ");
+  script.Print("                                            ");
+  script.Print("********************************************");
+  script.Print("*     	        By TEAM-AIM                *");
+  script.Print("********************************************");
+  script.Print(" ")
   device_specific.FullOTA_InstallBegin()
 
   # All other partitions as well as the data wipe use 10% of the progress, and
